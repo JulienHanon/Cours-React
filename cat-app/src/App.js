255f66e8a23935d43738711925dc318aff3119b5
adrien <a.dubost@safetyn.com>
2022-01-04 17:34:26 +0100

    my-app : découverte React Routeur simuler un changement de page // pour Cat app : affichage d'une liste de chat avec nom, age, race, poids (Revoir la gestion des données es chats)

--- cat-app/src/App.js
+++ cat-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import CatList from './component/CatList';
+import Cat from './component/Cat.Js';
 
 function App() {
   return (
     <div className="App">
-      <CatList></CatList>
+      <CatList> 
+      </CatList>
     </div>
   );
 }
--- cat-app/src/component/CatList.js
+++ cat-app/src/component/CatList.js
@@ -1,6 +1,12 @@
 import './CatList.css';
 
 function CatList(){
+
+    const CatName = ["Neko", "Kira", "Osiris", "Venus", "Nirvana"]; 
+    const CatAge = [9, 1, 4, 2, 6]; 
+    const CatRace = ["Ragdoll", "MainCoon", "Sphinx", "Sacré de Birmanie", "Norvegien"]
+    const CatWeight = [3.2, 4.4, 4.2, 2.9, 3.4]; 
+
     return (
         <div className='List'>
             <header className="TitleList">
@@ -8,11 +14,11 @@ function CatList(){
             </header>
             <body>
                 <ul className="CatList">
-                    <li>I'm a cat</li>
-                    <li>I'm a cat</li>
-                    <li>I'm a cat</li>
-                    <li>I'm a cat</li>
-                    <li>I'm a cat</li>
+                    <li>{CatName[0]} - {CatAge[0].toString()} ans - {CatRace[0]} - {CatWeight[0].toString()}</li>
+                    <li>{CatName[1]} - {CatAge[1].toString()} ans - {CatRace[1]} - {CatWeight[1].toString()}</li>
+                    <li>{CatName[2]} - {CatAge[2].toString()} ans - {CatRace[2]} - {CatWeight[2].toString()}</li>
+                    <li>{CatName[3]} - {CatAge[3].toString()} ans - {CatRace[3]} - {CatWeight[3].toString()}</li>
+                    <li>{CatName[4]} - {CatAge[4].toString()} ans - {CatRace[4]} - {CatWeight[4].toString()}</li>
                     <p>          </p>
                 </ul>
             </body>
