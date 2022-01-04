b2fdb011dd4c75a3f63e3c8ed6aad8e233150745
JulienHanon <hanonjulien@outlook.fr>
2022-01-04 15:59:01 +0100

    test console.log renvoie des inputs

--- cat-app/src/Component/FormCat.js
+++ cat-app/src/Component/FormCat.js
@@ -3,10 +3,24 @@ import React, { useState } from 'react';
 
 
 function FormCat(){
+    
+    const name = "";
+    const age = 0;
+    const breed = "";
+    const height = 0;
+    const weight = 0; {
+        
+    }
 
     const formSubmissionHandler = event => {
         event.preventDefault();
         console.log("Form submitted !")
+        console.log(event.target.name.value)
+        console.log(event.target.age.value)
+        console.log(event.target.breed.value)
+        console.log(event.target.height.value)
+        console.log(event.target.weight.value)
+      
     };
 
     return(
@@ -16,7 +30,7 @@ function FormCat(){
            <form onSubmit={formSubmissionHandler}>
                <label>
                     Nom :
-                    <input id='name' type="text" name="name" />
+                    <input id={name} type="text" name="name" />
                     Age : 
                     <input id='age' type="number" name="Age"/>
                     Race : 
