bf747520c156b7fd0883055d78d0ee16fdf73561
adrien <a.dubost@safetyn.com>
2022-01-05 09:44:26 +0100

    Modification CatList : création d'un tableau de chat avec leur caractéristiques

--- cat-app/src/component/CatList.js
+++ cat-app/src/component/CatList.js
@@ -1,11 +1,31 @@
 import './CatList.css';
+import Cat from './Cat.Js';
 
-function CatList(){
+function CatList(props){
 
-    const CatName = ["Neko", "Kira", "Osiris", "Venus", "Nirvana"]; 
-    const CatAge = [9, 1, 4, 2, 6]; 
-    const CatRace = ["Ragdoll", "MainCoon", "Sphinx", "Sacré de Birmanie", "Norvegien"]
-    const CatWeight = [3.2, 4.4, 4.2, 2.9, 3.4]; 
+    const CAT = [
+        {
+            Id: 'C1', 
+            Name: 'Neko',
+            Age:  9, 
+            Race: 'RagDoll',
+            Weight: 3.4
+        },
+        {
+            Id: 'C2',
+            Name: 'Kira', 
+            Age: 1, 
+            Race: 'MainCoon', 
+            Weight: 4.1
+        }, 
+        {
+            Id: 'C3', 
+            Name: 'Osiris', 
+            Age: 4, 
+            Race: 'Sphinx', 
+            Weight: 2.9
+        }
+    ]
 
     return (
         <div className='List'>
@@ -14,12 +34,9 @@ function CatList(){
             </header>
             <body>
                 <ul className="CatList">
-                    <li>{CatName[0]} - {CatAge[0].toString()} ans - {CatRace[0]} - {CatWeight[0].toString()}</li>
-                    <li>{CatName[1]} - {CatAge[1].toString()} ans - {CatRace[1]} - {CatWeight[1].toString()}</li>
-                    <li>{CatName[2]} - {CatAge[2].toString()} ans - {CatRace[2]} - {CatWeight[2].toString()}</li>
-                    <li>{CatName[3]} - {CatAge[3].toString()} ans - {CatRace[3]} - {CatWeight[3].toString()}</li>
-                    <li>{CatName[4]} - {CatAge[4].toString()} ans - {CatRace[4]} - {CatWeight[4].toString()}</li>
-                    <p>          </p>
+                    <li>{CAT[0].Name} - {CAT[0].Age.toString()} ans - {CAT[0].Race} - {CAT[0].Weight.toString()}kg</li>
+                    <li>{CAT[1].Name} - {CAT[1].Age.toString()} ans - {CAT[1].Race} - {CAT[1].Weight.toString()}kg</li>
+                    <li>{CAT[2].Name} - {CAT[2].Age.toString()} ans - {CAT[2].Race} - {CAT[2].Weight.toString()}kg</li>
                 </ul>
             </body>
         </div>
