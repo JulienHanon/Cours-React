1009a731854423222b03fd402959857d577b7c03
JulienHanon <hanonjulien@outlook.fr>
2022-01-06 14:25:14 +0100

    race dans le menu déroulant du formulaire depuis l'API

--- cat-app/src/routes/FormCat.js
+++ cat-app/src/routes/FormCat.js
@@ -1,7 +1,36 @@
 import './../UI/Card.css';
 
+import React, { useState, useEffect } from 'react';
+
 function FormCat(props){
 
+    const[breed, setBreed] = useState(['']);
+
+    const fetchCatBreed = async () =>{
+        const response = await fetch('https://api.thecatapi.com/v1/breeds');
+        const data = await response.json();  
+        const transformedBreed = data.map((breedData) =>{
+            return{
+                id : breedData.id,
+                breed : breedData.name,
+            };
+        });
+        setBreed(transformedBreed); 
+        for(let i = 0; i<breed.length;i++){
+            console.log(breed[i].breed);
+        }
+           
+     }
+     useEffect(() => {
+        let timer = setTimeout(() => {
+        fetchCatBreed();
+        }, 0);
+        
+        
+        
+        return () => clearTimeout(timer)
+        }, []);
+     
 
     const formSubmissionHandler = (event) => {
         event.preventDefault();
@@ -26,9 +55,9 @@ function FormCat(props){
                     <input id='age' type="number" name="Age"/>
                     Race : 
                     <select id='breed'>
-                        <option>RagDoll</option>
-                        <option>MainCoon</option>
-                        <option>Sphinx</option>
+                        <option>{breed[0].breed}</option>
+                        <option>{breed[1].breed}</option>
+                        <option>{breed[2].breed}</option>
                     </select>
                     Taille :
                     <input id='height' type="number" name="Taille"/>
